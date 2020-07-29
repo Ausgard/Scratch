2753634833a976340db0215a3bb9dcda154785f9
Ausgard <48877786+Ausgard@users.noreply.github.com>
2020-07-29 15:16:07 +0300

    Обработчик кнопки recipe-save

--- src/assets/js/recipe-save-handler.js
+++ src/assets/js/recipe-save-handler.js
@@ -1,8 +1,17 @@
-let saveBtn = $('.content__recipe-save')
-// saveBtn.html('saved')
-saveBtn.bind('click', handler(event))
-// $('.content__recipe-save').click(handler(event))
+$(document).ready(function(){
+    let saveBtn = $('.content__recipe-save')
+    saveBtn.on('click', setBtnValue)
 
-function handler(event) {
-    console.log(event)
-}
+    function setBtnValue(event) {
+        $(this).html('Saved')
+        $(this).css('justify-content', 'center')
+        $(this).css('background-color', '#30BE76')
+        $(this).css('color', '#ffffff')
+        if($(event.target).html() === 'Saved') {
+            $(this).html('<svg><use xlink:href="assets/img/sprite.svg#icon-recipe-save"></use></svg><span>Save</span>')
+            $(this).css('justify-content', 'space-between')
+            $(this).css('background-color', '#ffffff')
+            $(this).css('color', '#30BE76')
+        }
+    }
+})
